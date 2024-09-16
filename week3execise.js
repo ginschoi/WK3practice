392b5b8656294f8e9ae2b59d1b0ee18caa4a2da3
ginschoi <166687384+ginschoi@users.noreply.github.com>
2024-09-16 10:45:51 -0700

    Add files via upload

--- week3execise.js
+++ week3execise.js
@@ -1,16 +1,20 @@
 // PLANNING //
 
-// What are you planning to write for your console.log? = I am going to write about the upcoming plans for the weekend about a friend named Jereon and his birthday celebration plans to go to a karaoke bar.
+// What are you planning to write for your console.log? = I am going to write about the upcoming plans for the weekend about a friend named Jereon and his birthday celebration plans to go to a karaoke bar. His age is turning from 26 to 27 so I will use that for the changing number variable.
 
-// What kind of types are you going to use and where? (using both number, string is requirement) = I'll use constant string variable of birthdayfriend and let number variable for his age that will be changing from 26 to 27
+// What kind of types are you going to use and where? (using both number, string is requirement) = I'll use constant string variable of birthdayfriend for my friend's name that won't be changing throughout the story and will declare the let variable as a number for his age that will be changing from 26 to 27.
 
 const BirthdayFriend = "Jereon"
 
 let FriendsAge = 26
+let 
 
 console.log("This weekend, I plan to spend time with a big group of friends to celebrate the birthday of a close friend and former housemate, " +BirthdayFriend+ ", who is currently " +FriendsAge+ ". " +BirthdayFriend+ " has thoroughly enjoyed being " +FriendsAge+ ", and hoping to launch into his new age with a bunch of our friends.  First, I’ll meet up with other friends and " +BirthdayFriend+ " for predrinks with cheap beers at the Metropole downtown. After drinks, we’ll head to a karaoke bar called Funky Winkerbeans, where my friends and I sometimes hang out, for the janky vibes and good times.");
 
 FriendsAge = 27
 
-console.log ("Our friend " +BirthdayFriend+ " is a bit nervous about turning " +FriendsAge+ ", since it will mean he is getting closer to his thirties. " +FriendsAge+ " is not even that old, but he is understandably getting in his head about getting older and never having had a girlfriend. We will be there to support "+BirthdayFriend+" in his feels about our aging bodies, and maybe we can talk him through in realizing that his self worth should not be based on his dating history, nor his new age of " +FriendsAge+ ".");
+console.log ("Our friend " +BirthdayFriend+ " is a bit nervous about turning " +FriendsAge+ ", since it will mean he is getting closer to his thirties. " +FriendsAge+ " is not even that old, but he is understandably getting in his head about getting older and never having had a girlfriend. We will be there to support "+BirthdayFriend+" in his feels about our aging bodies, and maybe we can talk him through in realizing that he is a great person and friend, meaning that his self worth should not be based on the number of past girlfriends, nor his new age of " +FriendsAge+ ".");
 
+// Looks good but not sure if he wants a let variable that is also a string aside from a number. example: let food = "cake"
+// Also add a new console.log to split messages example: console.log("----------2nd Story"-----------)
+//Kyle 
